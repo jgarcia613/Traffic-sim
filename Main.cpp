30694622297244670f794019655451ba1c885255
WesStutzman <wstutzman@student.cccs.edu>
2016-09-20 16:58:55 -0600

    Update Main.cpp
    New main for testing new acceleration classes.
    Now we have the ability to create arrays of cars and test all of them with one function.

--- Main.cpp
+++ Main.cpp
@@ -1,46 +1,71 @@
-//************************************
-//Acceleration Traifc Simulation
-//************************************
-#include <iostream>
-#include <string>
-#include "Vehicle.h"
+//******************
+//Trafic Simulation
+//******************
+#include "Includes.h"
 
 int timeOfDay = 0;
 
 ostream &operator<<(ostream &output, Vehicle inputVehicle)
 {
-	using namespace std;
-	
-	output << "Seconds Passed:\t" << timeOfDay++ << endl;
+	output << "Time:\t\t12:" << timeOfDay << endl;
 	output << "Model:\t\t" << inputVehicle.getModel() << endl;
 	output << "Latitude:\t" << inputVehicle.getlatitude() << endl;
 	output << "Longitude:\t" << inputVehicle.getLongitude() << endl;
 	output << "Current Speed:\t" << inputVehicle.getCurrentSpeed() << endl;
 	output << endl << endl;
-	system("pause");
-	system("cls");
 	return output;
 }
 
 int main()
 {
-
-	Vehicle myCar;										//first create a car
-	myCar.setlatitude(0);								//set its starting values
-	myCar.setLongitude(0);								//**
-	myCar.setDirection(1);								//**
-	myCar.setCurrentSpeed(0);							//**
-	myCar.setAccelerationSpeed(5);						//**
-	myCar.setAcceleration(2);							//**
-	myCar.setbrakingSpeed(5);							//**
-	myCar.setTime(1);									//**
-	myCar.setModel("Honda");							//**
-	myCar.setCurrentSpeed(1);							//**
-	myCar.setMaxSpeed(60);								//**
-
-	while(true)
+	Vehicle myCar(MIDSIZE);									//first create a car
+	myCar.setlatitude(40.581503000);						//set its starting variables
+	myCar.setLongitude(-105.062886);						//**
+	myCar.setCurrentSpeed(1);								//**
+	myCar.setAccelerationSpeed(3);							//**
+	myCar.setbrakingSpeed(5);								//**
+	myCar.setMaxSpeed(60);
+	cout << myCar;
+	myCar.setModel("Honda");
+	myCar.setDirection(140);
+	VehicleComputer carComputer(TIMEINCREMENT);
+	//test the acceleration of the car
+	cout << "Starting testAcceleration." << endl;			//prompt the users
+	system("pause");
+	for(int i = 0; i < 50; i++)
 	{
-		++myCar;
+		carComputer.moveVehicle(myCar);
+		system("pause");
+		system("cls");
 		cout << myCar;
+		timeOfDay++;
 	}
-}//End of main
+
+	//Testing moving arrays full of cars
+	Vehicle* testCars;
+ 	testCars = new Vehicle[5];
+	for (int i = 0; i < sizeof(testCars); i++)
+	{
+		testCars[i].setlatitude(40.581503000);						//set its starting variables
+		testCars[i].setLongitude(-105.062886);						//**
+		testCars[i].setCurrentSpeed(1);								//**
+		testCars[i].setAccelerationSpeed(3);						//**
+		testCars[i].setbrakingSpeed(5);								//**
+		testCars[i].setMaxSpeed(60);
+		testCars[i].setModel("Honda");
+		testCars[i].setDirection(140);
+	}
+	VehicleComputer carHub(TIMEINCREMENT);
+
+	for (int i = 0; i < 50; i++)
+	{
+
+		carComputer.moveVehicle(testCars);
+
+		system("pause");
+ 		
+		for (int m = 0; m < sizeof(testCars); m++)
+			cout << testCars[m];
+		timeOfDay++;
+	}
+}
