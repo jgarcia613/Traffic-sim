c5af6d4301a919c61f4060a180f60825f806e56a
David-Sant <dsant@student.cccs.edu>
2016-10-02 16:45:09 -0600

    Add files via upload

---
+++ SignalDemo.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+
+/* class to input data about a specific signal at a specxific time interval*/
+
+class Signal
+{
+private:
+	
+
+	string location;	// college and drake
+
+	double latitude;
+	double longitude;
+
+	int startTime; //0730 hours
+	int endTime;
+
+	//car count totals for thru lane//
+	int southBoundCount;
+	int westBoundCount;
+	int northBoundCount;
+	int eastBoundCount;
+public:
+	Signal();
+	void vectorFill(ifstream &fin);		
+	void printVector();
+
+};
