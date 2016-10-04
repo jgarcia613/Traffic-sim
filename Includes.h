7b3517a2442557e14c3562e1ff787c9e53b9e1a7
spencemw <swilson246@student.cccs.edu>
2016-10-04 10:28:57 -0600

    Line 8 Error
    //#define PI is using the same name 'PI' here, commented out (Spence
    Wilson 0945 10/4/16)
    
    #define PI is located on line 8 of ‘vehicle.h’.  This works the same
    was as const double PI listed here.

--- Includes.h
+++ Includes.h
@@ -5,6 +5,6 @@
 using namespace std;
 #include "Vehicle.h"
 #include "VehicleComputer.h"
-const double PI = 3.14;
+//const double PI = 3.14;  //#define PI is using the same name 'PI' here, commented out (Spence Wilson 0945 10/4/16)
 const int TIMEINCREMENT = 1;
 const enum CARTYPES {MIDSIZE, SEMI, TRUCK, SPORTSCAR};
