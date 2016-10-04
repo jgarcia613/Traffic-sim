ab11684404512857d9caced8ecbdf6ea4b05ab12
csc233 <mikeritter1993@gmail.com>
2016-10-04 14:39:14 -0600

    Update Includes.h

--- Includes.h
+++ Includes.h
@@ -4,7 +4,6 @@
 #include <Math.h>
 using namespace std;
 #include "Vehicle.h"
-#include "VehicleComputer.h"
 const double PI = 3.14;
 const int TIME_INCREMENT = 1;
 const enum CARTYPES {MIDSIZE, SEMI, TRUCK, SPORTSCAR};
